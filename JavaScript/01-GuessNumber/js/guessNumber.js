a27dd2c51db84a6639a2dc94833743348d20d05e
yisus82 <djalma8@gmail.com>
2021-10-08 10:44:23 +0200

    Refactoring and minor changes

--- JavaScript/01-GuessNumber/js/guessNumber.js
+++ JavaScript/01-GuessNumber/js/guessNumber.js
@@ -1,56 +1,61 @@
-const result = document.getElementById('result');
+const message = document.getElementById('message');
 const numberForm = document.getElementById('numberForm');
 const userNumberField = document.getElementById('userNumberField');
 const submitButton = document.getElementById('submitButton');
-const newButton = document.getElementById('newButton');
+const newNumberButton = document.getElementById('newNumberButton');
 const attemptList = document.getElementById('attemptList');
 const optionButton = document.getElementById('optionButton');
 let maxAttempts = 3;
-let numAttempts = 0;
+let currentAttempts = 0;
 let min = 1;
 let max = 10;
 
-const generateRandomNumber = (min = 1, max = 10) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
+const generateRandomNumber = (min = 1, max = 10) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
 
 let randomNumber = generateRandomNumber(min, max);
 console.log(randomNumber);
 
+const endGame = () => {
+  submitButton.disabled = true;
+  newNumberButton.style.display = 'initial';
+};
+
+const checkNumber = number => {
+  if (number < randomNumber) {
+    message.innerHTML = 'Random number is higher';
+  } else if (number > randomNumber) {
+    message.innerHTML = 'Random number is lower';
+  } else {
+    message.innerHTML = `Random number guessed correctly in ${currentAttempts} attempt(s)!`;
+    endGame();
+  }
+  if (currentAttempts >= maxAttempts) {
+    message.innerHTML = `Random number was ${randomNumber}`;
+    endGame();
+  }
+};
+
 const submitNumber = event => {
   event.preventDefault();
   const userNumber = +userNumberField.value;
-  numAttempts++;
+  userNumberField.value = '';
+  currentAttempts++;
   attemptList.innerHTML += `<li>${userNumber}</li>`;
-  if (userNumber < randomNumber) {
-    result.innerHTML = "Random number is higher";
-  } else if (userNumber > randomNumber) {
-    result.innerHTML = "Random number is lower";
-  } else {
-    result.innerHTML = `Random number guessed correctly in ${numAttempts} attempt(s)!`;
-    submitButton.disabled = true;
-    newButton.style.display = "initial";
-  }
-  if (numAttempts === maxAttempts) {
-    submitButton.disabled = true;
-    newButton.style.display = "initial";
-    result.innerHTML = `Random number was ${randomNumber}`;
-  }
+  checkNumber(userNumber);
 };
-
 numberForm.onsubmit = submitNumber;
 
 const generateNewNumber = () => {
-  result.innerHTML = "";
-  attemptList.innerHTML = "";
-  numAttempts = 0;
+  message.innerHTML = '';
+  attemptList.innerHTML = '';
+  currentAttempts = 0;
   randomNumber = generateRandomNumber(min, max);
   console.log(randomNumber);
   submitButton.disabled = false;
-  newButton.style.display = "none";
+  newNumberButton.style.display = 'none';
 };
-
-newButton.onclick = generateNewNumber;
+newNumberButton.onclick = generateNewNumber;
 
 const selectOption = () => {
   const radioBtnEasy = document.getElementById('easy');
@@ -66,5 +71,4 @@ const selectOption = () => {
   }
   generateNewNumber();
 };
-
 optionButton.onclick = selectOption;
